696b33decfa295a7211c5a148ababa15e63b5e6b
eslasojica <keslasojica@gmail.com>
2025-07-14 06:52:17 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Smk';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Jepang Viral Sma Cindo Tante China';
-export const url = 'https://bokepsmk.pages.dev';
-export const terbit = '2025-06-25T00:01:03Z';
